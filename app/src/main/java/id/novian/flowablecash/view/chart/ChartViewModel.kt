a3f41b819352fa431cb5a85a377436660ed4e7e9
Novian Rachman Iskandar <novian19@upi.edu>
2023-07-31 03:32:28 +0700

    REFACTOR: Chart Fragment with transactions (pemasukkan and pengeluaran)

--- app/src/main/java/id/novian/flowablecash/view/chart/ChartViewModel.kt
+++ app/src/main/java/id/novian/flowablecash/view/chart/ChartViewModel.kt
@@ -4,10 +4,11 @@ import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import id.novian.flowablecash.base.vm.BaseViewModel
 import id.novian.flowablecash.domain.models.AccountDomain
+import id.novian.flowablecash.domain.models.TransactionDomain
 import id.novian.flowablecash.domain.repository.AccountsRepository
-import io.reactivex.rxjava3.core.Observable
+import id.novian.flowablecash.domain.repository.TransactionRepository
+import id.novian.flowablecash.helpers.CalendarHelper
 import io.reactivex.rxjava3.core.Scheduler
-import io.reactivex.rxjava3.functions.Function3
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -15,36 +16,106 @@ import javax.inject.Named
 class ChartViewModel @Inject constructor(
     private val repo: AccountsRepository,
     @Named("IO") private val schedulerIo: Scheduler,
-    @Named("MAIN") private val schedulerMain: Scheduler
+    @Named("MAIN") private val schedulerMain: Scheduler,
+    private val transactions: TransactionRepository,
+    val calendarHelper: CalendarHelper
 ) : BaseViewModel() {
     var kasAccounts: MutableLiveData<List<AccountDomain>> = MutableLiveData()
         private set
 
-    var penjualanAccounts: MutableLiveData<List<AccountDomain>> = MutableLiveData()
+    var pemasukkanData: MutableLiveData<List<TransactionDomain>> = MutableLiveData()
         private set
 
-    var pembelianAccounts: MutableLiveData<List<AccountDomain>> = MutableLiveData()
+    var pengeluaranData: MutableLiveData<List<TransactionDomain>> = MutableLiveData()
         private set
 
     override fun viewModelInitialized() {
         getAllNeededAccounts()
+        getAllTransactions()
     }
 
     private fun getAllNeededAccounts() {
-        val disposable = Observable.zip(
-            repo.getAllSpecific("Kas").subscribeOn(schedulerIo),
-            repo.getAllSpecific("Penjualan").subscribeOn(schedulerIo),
-            repo.getAllSpecific("Pembelian").subscribeOn(schedulerIo),
-            Function3 { kas, penjualan, pembelian ->
-                Triple(kas, penjualan, pembelian)
-            }
-        )
+        val disposable = repo.getAllSpecific("Kas").subscribeOn(schedulerIo)
             .subscribeOn(schedulerIo)
             .observeOn(schedulerMain)
-            .subscribe({ (kas, penjualan, pembelian) ->
+            .subscribe({ kas ->
                 kasAccounts.postValue(kas)
-                penjualanAccounts.postValue(penjualan)
-                pembelianAccounts.postValue(pembelian)
+            }, { err ->
+                err.printStackTrace()
+                errorMessage.postValue(err.message)
+            })
+
+        compositeDisposable.add(disposable)
+    }
+
+    private fun getAllTransactions() {
+        val disposable = transactions.getAllTransactions()
+            .subscribeOn(schedulerIo)
+            .observeOn(schedulerMain)
+            .subscribe({ (pemasukkan, pengeluaran) ->
+
+                val filteredOneMonthBeforePemasukkan = pemasukkan
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth() - 1
+                    }
+
+                val filteredCurrentPemasukkkan = pemasukkan
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth()
+                    }
+
+                val filteredOneMonthAfterPemasukkan = pemasukkan
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth() + 1
+                    }
+
+                val filteredOneMonthBeforePengeluaran = pengeluaran
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth() - 1
+                    }
+
+                val filteredCurrentPengeluaran = pengeluaran
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth()
+                    }
+
+                val filteredOneMonthAfterPengeluaran = pengeluaran
+                    .filter {
+                        val parts = it.transactionDate.split("-")
+                        val months = if (parts.size == 3) {
+                            parts[1].toInt()
+                        } else -1
+
+                        months == calendarHelper.getMonth() + 1
+                    }
+
+                pemasukkanData.postValue(filteredOneMonthBeforePemasukkan + filteredCurrentPemasukkkan + filteredOneMonthAfterPemasukkan)
+                pengeluaranData.postValue(filteredOneMonthBeforePengeluaran + filteredCurrentPengeluaran + filteredOneMonthAfterPengeluaran)
             }, { err ->
                 err.printStackTrace()
                 errorMessage.postValue(err.message)
