b7ddfda989250487d7901098b865b3cf20160ceb
Novian Rachman Iskandar <novian19@upi.edu>
2023-08-07 12:18:56 +0700

    REFACTOR: SplashActivity and Launcher needs (icon, app_name)

--- app/src/main/java/id/novian/flowablecash/base/layout/SplashActivity.kt
+++ app/src/main/java/id/novian/flowablecash/base/layout/SplashActivity.kt
@@ -5,15 +5,18 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import id.novian.flowablecash.R
+import id.novian.flowablecash.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
 
     private val splashTimeOut = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
